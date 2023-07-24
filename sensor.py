9d5e618537876a35301c5ef5f2cca9fc274b753e
TheKonamiKoder <thekonamicode02@gmail.com>
2023-07-24 04:29:24 +0100

    Create sensor module

--- sensor.py
+++ sensor.py
@@ -3,29 +3,38 @@ import time
 
 from enum import Enum
 
+from typing import Optional
+
 
 class SensorType(Enum):
     LIGHT_SENSOR = 0
     TEMPERATURE_SENSOR = 1
     HUMIDITY_SENSOR = 2
+    MOTION_SENSOR = 3
 
 
-class LightSensorData:
+class SensorData:
+    pass
+
+
+class LightSensorData(SensorData):
     def __init__(self, val: int = 0) -> None:
         self.val = val
 
 
-class TemperatureSensorData:
+class TemperatureSensorData(SensorData):
     def __init__(self, val: float = 0.0) -> None:
         self.val = val
 
 
-class HumiditySensorData:
+class HumiditySensorData(SensorData):
     def __init__(self, val: float = 0.0) -> None:
         self.val = val
 
 
-SensorData = LightSensorData | TemperatureSensorData | HumiditySensorData | None
+class MotionSensorData(SensorData):
+    def __init__(self, val: float = False) -> None:
+        self.val = val
 
 
 class Sensor:
@@ -33,7 +42,7 @@ class Sensor:
         self.id = sensor_id
         self.type = sensor_type
 
-        self.data: SensorData = None
+        self.data: Optional[SensorData] = None
 
         if self.type == SensorType.LIGHT_SENSOR:
             self.data = LightSensorData()
@@ -43,6 +52,8 @@ class Sensor:
             self.data = HumiditySensorData()
 
     def get_data(self) -> SensorData:
+        # TODO: Make this function actually linked to the sensors
+
         time.sleep(secs=random.uniform(0, 3))
 
         if self.type == SensorType.LIGHT_SENSOR:
