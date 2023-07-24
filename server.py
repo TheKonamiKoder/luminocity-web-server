91b6e6c3de984717e28b4c4379beaec22118d846
TheKonamiKoder <thekonamicode02@gmail.com>
2023-07-24 09:07:28 +0100

    Use Sensor instead of dicts

--- server.py
+++ server.py
@@ -2,14 +2,40 @@ import random
 import time
 import json
 
+from typing import List
+
+import sensor
+
 from flask import Flask, request
 
 app = Flask(__name__)
 app.debug = True  # For debugging
 
 
-raw_data = '{"sensor1": 53, "sensor2": 43}'
-data: dict = json.loads(raw_data)
+sensors: List[sensor.Sensor] = [
+    sensor.Sensor(
+        sid=0,
+        name="Light Sensor",
+        room="Living Room",
+        stype=sensor.SensorType.LIGHT_SENSOR,
+    ),
+    sensor.Sensor(
+        sid=1,
+        name="Temp Sensor",
+        room="Garden",
+        stype=sensor.SensorType.TEMPERATURE_SENSOR,
+    ),
+    sensor.Sensor(
+        sid=2,
+        name="Humidity Sensor",
+        room="Kitchen",
+        stype=sensor.SensorType.HUMIDITY_SENSOR,
+    ),
+]
+
+# Populate sensors with data
+for _sensor in sensors:
+    _sensor.get_data()
 
 
 @app.route("/")
@@ -19,12 +45,12 @@ def home() -> str:
 
 @app.route("/get_all")
 def get_all() -> str:
-    return str(data)
+    return json.dumps(sensors, cls=sensor.SensorEncoder)
 
 
 @app.route("/get_sensor_ids")
 def get_sensor_ids() -> str:
-    return str([sensor_name for sensor_name in data])
+    return str([_sensor.id for _sensor in sensors])
 
 
 @app.route("/get_sensor_data")
@@ -37,15 +63,23 @@ def get_with_id() -> str:
     return "sensor_id must be supplied"
 
 
-@app.route("/add_sensor", methods=["GET"])
+@app.route("/add_sensor", methods=["UPDATE"])
 def add_sensor() -> str:
-    sensor_id: str = request.args.get("sensor_id", None)
-    sensor_type: str = request.args.get("sensor_type", None)
+    sid: str = request.args.get("sid", None)
+    name: str = request.args.get("name", None)
+    room: str = request.args.get("room", None)
+    stype: str = request.args.get("stype", None)
 
-    if sensor_id and sensor_type:
-        data[sensor_id] = Sensor.get_data(sensor_id)
+    if sid and name and room and stype:
+        for _stype in sensor.SensorType:
+            if _stype.name == stype:
+                __stype = _stype
 
-        return "Success"
+        sensors.append(sensor.Sensor(int(sid), name, room, __stype))
+
+        return "success"
+    else:
+        return "error - all parameters not supplied"
 
 
 if __name__ == "__main__":
